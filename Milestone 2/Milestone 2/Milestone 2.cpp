be5754dcda0490acad8d8c49f8017556872480ee
Shep Willl <70408297+peach-n-creme@users.noreply.github.com>
2022-03-28 19:19:48 -0400

    mimi

--- Milestone 2/Milestone 2/Milestone 2.cpp
+++ Milestone 2/Milestone 2/Milestone 2.cpp
@@ -84,7 +84,7 @@ bool SetStart(int xpos, int ypos)
     startPosX = xpos;
     startPosY = ypos;
 
-    if ()
+    if (startPosX == xpos && startPosY == ypos && startPosX >=0 && startPosY >= 0)
     {
         return true;
     }
@@ -96,7 +96,7 @@ bool SetStart(int xpos, int ypos)
 
 bool GetStart(int& xpos, int& ypos)
 {
-    if (startPosX == -1 && startPosY == -1)
+    if (startPosX == xpos && startPosY == ypos && startPosX >= 0 && startPosY >= 0)
     {
         xpos = -1;
         ypos = -1;
